419e402b9a52fec9ed141ee7b42ae8b4a300c5db
Pooja <Pooja@DESKTOP-8CI8H38>
2023-10-30 13:03:58 +0530

    By pranaya

---
+++ NewGit/src/Demo1/GitDemo1.java
@@ -0,0 +1,14 @@
+package Demo1;
+
+public class GitDemo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello...");
+
+	}
+	
+	public void m1() {
+		System.out.println("This Is...Pranay..");
+	}
+
+}
